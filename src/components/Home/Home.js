b65d2dc5a42bb5e35066eb78f9fc90fcc65becb1
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-07 20:38:19 -0800

    existing product added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,8 +8,15 @@ const Home = () => {
     const tshirts = useLoaderData();
     const [cart, setCart] = useState([]);
     const handleAddToCart = tshirt =>{
-        const newCart = [...cart, tshirt];
-        setCart(newCart);
+        const exists = cart.find(ts => ts._id === tshirt._id)
+        if (exists) {
+            alert('t-shirt already added')
+        } else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+            //alert('successfully added')
+        }
+        
     }
     return (
         
